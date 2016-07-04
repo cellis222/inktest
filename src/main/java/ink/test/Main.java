ad5847bee358fb4501f21f15fe588005cd75af3f
Craig <cellis_222@hotmail.com>
2016-07-04 11:50:13 +0100

    Maven setup, Main class + Main test

---
+++ src/main/java/ink/test/Main.java
@@ -0,0 +1,14 @@
+package ink.test;
+
+/**
+ *
+ * @author Craig
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Application has not been written yet");
+        System.exit(0);
+    }
+
+}
---
+++ src/test/java/ink/test/MainTest.java
@@ -0,0 +1,17 @@
+package ink.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Craig
+ */
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        fail("The test case is a prototype.");
+    }
+
+}
